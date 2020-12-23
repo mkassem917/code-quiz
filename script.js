eb00727f5479c8a331252c2f6398275b06b3d1e6
Moe Kassem <74279957+mkassem917@users.noreply.github.com>
2020-12-22 22:38:14 -0500

    updated style.css file & started on script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// ID Elements
+var startQuiz = document.querySelector("#startquiz");
+
+var startQuizBtn = document.querySelector("#startbtn");
+var highScoreBtn = document.querySelector("#homePageHighscore");
+var quizEl = document.querySelector("quiz");
+var timerEl = document.querySelector("timer");
+var questionsEl = document.querySelector("questions");
+var btnA
+
+
+
+
+var resultsEL = document.querySelector("results");
